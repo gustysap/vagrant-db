2f86a08a7b2f1d6de30787b7205c333aa5472e8c
Gusty Sapto Ady Prakoso <gusty.prakoso@flip.id>
2021-12-12 10:24:16 +0700

    update database.sh

--- Vagrantfile
+++ Vagrantfile
@@ -2,10 +2,10 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", path: "database.sh"
   config.vm.define "database" do |database|
     database.vm.box = "ubuntu/xenial64"
-    database.vm.network "private_network",ip: "10.168.8.10"
+    database.vm.network "private_network",ip: "192.168.56.56"
       database.vm.provider "virtualbox" do |v|
-        v.memory = 1024
-        v.cpus = 1
+        v.memory = 2048
+        v.cpus = 2
      end
   end
 end
--- database.sh
+++ database.sh
@@ -1,9 +1,30 @@
-#!/bin/bash
+!/bin/bash
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password ganteng'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ganteng'
 echo "Menyiapkan Installasi Database Server"    
 sudo apt-get update    
-echo "Melakukan Installasi Database Server"    
+
+echo "Melakukan Installasi Database Server"
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password 123'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 123'    
 sudo apt-get install -y mysql-server    
 echo "Installasi Selesai"    
+
+echo "Konfigurasi MySQL pada File /etc/mysql/mysql.conf.d/mysqld.cnf"
+sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
+sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "Update bind address MySQL pada File /etc/mysql/mysql.conf.d/mysqld.cnf agar mengizinkan koneksi eksternal"
+
+echo "Restart Service Database Server"
+sudo service mysql stop
+sudo service mysql start
+echo "Konfigurasi MySQL Selesai"
+
+echo "CREATE DATABASE & USER"
+sudo mysql -u root -p123 << EOF
+CREATE DATABASE IF NOT EXISTS dbsosmed;
+CREATE DATABASE IF NOT EXISTS wordpress_db;
+CREATE USER IF NOT EXISTS 'devopscilsy'@'%' IDENTIFIED BY '1234567890';
+GRANT ALL PRIVILEGES ON *.* TO 'devopscilsy'@'%';
+FLUSH PRIVILEGES;
+EOF
+echo "Create Database & User Selesai"
